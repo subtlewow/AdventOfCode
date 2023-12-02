84f61e69b7a3601a306c5a7324c52d60b5061809
Marlon Chhour <bane6459@gmail.com>
2023-12-02 14:54:07 +1300

    nearly finish day1 pt2

--- day1.py
+++ day1.py
@@ -1,20 +1,55 @@
 new = ""
 count = 0
-with open('temp.txt') as f:
-    for line in f:
-        tmp = list(line.strip('\n'))
-        
-        for i, char in enumerate(tmp):
-            ascii_val = ord(char)
-            if (ascii_val >= 48 and ascii_val <= 57):
-                if len(new) >= 2:
-                    new = str(int(new) // 10)
-                new += char
-        
-        if len(new) == 1:
-            new = new * 2
-        
-        count += int(new)
-        new = ""
+word_count = 0
 
-    print("The sum of all calibration values: {}".format(count))
+mapping = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+with open('day1.txt') as f:
+    # for line in f:
+        # tmp = list(line.strip('\n'))
+        tmp = "eightwothree"
+        key = ""
+        
+        for char in tmp:
+            key += char
+            
+            if key in mapping:
+                tmp = tmp.replace(key, "")
+                word_count += mapping[key]
+                key = ""
+            else:
+                for mkey in mapping:
+                    if mkey in tmp:
+                        tmp = tmp.replace(mkey, "")
+                        word_count += mapping[mkey]
+                        mkey = ""
+
+                
+            
+            
+        print(tmp, word_count)
+        
+        # for char in line:
+        #     ascii_val = ord(char)
+        #     if (48 <= ascii_val <= 57):
+        #         if len(new) >= 2:
+        #             new = str(int(new) // 10)
+        #         new += char
+        
+        # if len(new) == 1:
+        #     new = new * 2
+        
+        # count += int(new)
+        # new = ""
+
+    # print("The sum of all calibration values: {}".format(count))
