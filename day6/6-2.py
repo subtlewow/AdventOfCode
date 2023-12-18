800ed6193c631b72be3088bf952c952341712f16
Marlon Chhour <marlon1293@gmail.com>
2023-12-18 16:28:37 +1300

    completed day6, used quad. formula

---
+++ day6/6-2.py
@@ -0,0 +1,12 @@
+import math
+
+x = open('./inputs/day6.txt').read().split('\n')
+z = [int("".join(xi.split(':')[1].split())) for xi in x]
+
+t, d = z
+c = 0
+
+x1 = math.ceil((-t / 2) - math.sqrt((t**2) / 4 - d))
+x2 = math.floor((-t / 2) + math.sqrt((t**2) / 4 - d))
+
+print(x2-x1+1)
--- day6/6.py
+++ day6/6.py
@@ -6,7 +6,6 @@ z = [xi.split(':')[1].split() for xi in x]
 items = [[int(item) for item in inner_ls] for inner_ls in z]
 td_pairs = list(zip(*items))
 
-c = 0
 final = 1
 for t, d in td_pairs:
     c = 0
