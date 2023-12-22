56b99dff8c0da52ed5cc77e519859efb03ade465
Marlon Chhour <marlon1293@gmail.com>
2023-12-22 18:30:41 +1300

    some small modifs

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,69 @@
-
+class Game:
+    def __init__(self):
+        self.players = []
+        self.num_players = 0
+        self.setup_game()
+        
+    def setup_game(self):
+        welcome_screen = """
+        \033[32m
+            ╔════════════════════════════════════════════════════════════════════════════════════════╗
+            ║                                                                                        ║
+            ║                                                                                        ║
+            ║                                                                                        ║
+            ║        ████████╗██╗ ██████╗ ████████╗ █████╗  ██████╗ ████████╗ ██████╗ ███████╗       ║
+            ║        ╚══██╔══╝██║██╔════╝ ╚══██╔══╝██╔══██╗██╔════╝ ╚══██╔══╝██╔═══██╗██╔════╝       ║
+            ║           ██║   ██║██║         ██║   ███████║██║         ██║   ██║   ██║█████╗         ║
+            ║           ██║   ██║██║         ██║   ██╔══██║██║         ██║   ██║   ██║██╔══╝         ║
+            ║           ██║   ██║╚██████╗    ██║   ██║  ██║╚██████╗    ██║   ╚██████╔╝███████╗       ║
+            ║           ╚═╝   ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝       ║
+            ║                                                                                        ║
+            ║                                                                                        ║
+            ║                                Welcome to Tic Tac Toe!                                 ║
+            ║                            Press Enter to start the game                               ║
+            ║                                                                                        ║
+            ║                                                                                        ║
+            ╚════════════════════════════════════════════════════════════════════════════════════════╝
+        \033[0m
+        """
+        
+        
+        self.grid_size = int(input("Enter size of grid: "))
+        
+        while self.num_players < 2:
+            try:
+                self.num_players = int(input("How many players to play?: "))
+                
+                if self.num_players < 2:
+                    print("try again, players > 2")
+                
+            except ValueError:
+                print('try again?')
+            
+        self.board = Board(self.grid_size, self.grid_size)
+        
+        # check if grid size can accomodate chosen players
+        
+        
+                
+        for i in range(self.num_players):
+            player_name = input(f"Enter the name of Player {i+1}: ")
+            self.players.append(Player(player_name))
+        
+        print(welcome_screen)
+        input("Press Enter to continue.. ")
+        Board().print_grid()
+    
+    def main_logic():
+        pass
+    
 class Board:
     def __init__(self, cols: int = 3, rows: int = 3):
         assert cols == rows, "cols and rows must be same dimensions"
         
         self.rows = rows
         self.cols = cols
+        self.grid = [['' for _ in range(cols)] for _ in range(rows)]
         
     def print_grid(self):
         i = j = 0
@@ -36,51 +95,15 @@ class Board:
                 
             print(connect)
             breaker = True
-        
-        
-class Game:
-    def __init__(self):
-        self.players = []
-        self.setup_game()
-        
-    def setup_game(self):
-        welcome_screen = """
-        \033[32m
-            ╔════════════════════════════════════════════════════════════════════════════════════════╗
-            ║                                                                                        ║
-            ║                                                                                        ║
-            ║                                                                                        ║
-            ║        ████████╗██╗ ██████╗ ████████╗ █████╗  ██████╗ ████████╗ ██████╗ ███████╗       ║
-            ║        ╚══██╔══╝██║██╔════╝ ╚══██╔══╝██╔══██╗██╔════╝ ╚══██╔══╝██╔═══██╗██╔════╝       ║
-            ║           ██║   ██║██║         ██║   ███████║██║         ██║   ██║   ██║█████╗         ║
-            ║           ██║   ██║██║         ██║   ██╔══██║██║         ██║   ██║   ██║██╔══╝         ║
-            ║           ██║   ██║╚██████╗    ██║   ██║  ██║╚██████╗    ██║   ╚██████╔╝███████╗       ║
-            ║           ╚═╝   ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝       ║
-            ║                                                                                        ║
-            ║                                                                                        ║
-            ║                                Welcome to Tic Tac Toe!                                 ║
-            ║                            Press Enter to start the game                               ║
-            ║                                                                                        ║
-            ║                                                                                        ║
-            ╚════════════════════════════════════════════════════════════════════════════════════════╝
-        \033[0m
-        """
-        
-        self.num_players = int(input("How many players to play?: "))
-        
-        for i in range(self.num_players):
-            self.player_name = input(f"Enter the name of Player {i+1}: ")
-            # self.players.append(Player(self.player_name))
-        
-        print(welcome_screen)
-        input("Press Enter to continue.. ")
     
-# class Player:
-#     def __init__(self):
+    def update_grid():
+        pass
         
     
+class Player:
+    def __init__(self, name):
+        self.name = name
     
     
-# Ask number of players, ask for player names
 
 Game()
